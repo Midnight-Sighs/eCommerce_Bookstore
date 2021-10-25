e39e8da95e29ddea7e881d70757b00faba15cc14
Midnight <arreiskurai@yahoo.com>
2021-10-25 14:46:48 -0700

    Added seed data for users.  Started controllers for users

---
+++ eCommerceStarterCode/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using eCommerceStarterCode.ActionFilters;
+using eCommerceStarterCode.Contracts;
+using eCommerceStarterCode.DataTransferObjects;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using eCommerceStarterCode.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        
+    }
+
+}
--- eCommerceStarterCode/Data/ApplicationDbContext.cs
+++ eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -13,12 +13,27 @@ namespace eCommerceStarterCode.Data
 
         }
 
+        public DbSet<User> User { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfiguration(new RolesConfiguration());
+            
+            
+            modelBuilder.Entity<User>().HasData(
+                new User { FirstName = "Chewbaca", LastName = "Solo", UserName = "Chewie", Email = "ChewieYou@aol.com", StreetAddress = "123 Millenium Falcon", City = "Some City", State = "No States Here", ZipCode = "12345"},
+                new User { FirstName = "Han", LastName = "Solo", UserName = "Han", Email = "IKnow@aol.com", StreetAddress = "123 Millenium Falcon", City = "Some City", State = "No States Here", ZipCode = "12345" },
+                new User { FirstName = "Joe", LastName = "Bishop", UserName = "BishopRocks", Email = "SkippySucks@aol.com", StreetAddress = "123 Valkyrie", City = "Roach Motel", State = "Galaxy Far Away", ZipCode = "12345" },
+                new User { FirstName = "Skippy", LastName = "The Magnificent", UserName = "Skippy_The_Mag", Email = "NoDirtyMonkeys@aol.com", StreetAddress = "123 Valkyrie", City = "Roach Motel", State = "Galaxy Far Away", ZipCode = "12345" }
+                );
+            
         }
+
+      
+
+
 
     }
 }
