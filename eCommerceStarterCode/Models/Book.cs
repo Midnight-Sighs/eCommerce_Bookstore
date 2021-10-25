9740ee67433983d7c5be77b69afee910467757fe
Gerard Mijares <gerard.mijares@outlook.com>
2021-10-25 23:52:06 +0300

    Modified Book.cs and Reviews.cs

--- eCommerceStarterCode/Models/Book.cs
+++ eCommerceStarterCode/Models/Book.cs
@@ -5,7 +5,32 @@ using System.Threading.Tasks;
 
 namespace eCommerceStarterCode.Models
 {
-    public class Class
+    public class Book
     {
+        [Key]
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        public string Genre { get; set; }
+
+        public string ReleaseYear { get; set; }
+
+        public string ISBN { get; set; }
+
+        public decimal Price { get; set; }
+
+
+        //FK to Users table
+        [ForeignKey("UserID")]
+
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
     }
 }
