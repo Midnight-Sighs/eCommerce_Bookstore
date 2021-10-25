ad761b51aedbe061260b3f83211acbd0fd6c328a
Midnight <arreiskurai@yahoo.com>
2021-10-25 13:02:09 -0700

    Made changes to the Users that we want to include with ours such as address (for purchasing products)

--- eCommerceStarterCode/Models/User.cs
+++ eCommerceStarterCode/Models/User.cs
@@ -6,5 +6,9 @@ namespace eCommerceStarterCode.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
     }
 }
